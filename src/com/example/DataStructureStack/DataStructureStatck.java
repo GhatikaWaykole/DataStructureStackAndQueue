cf44995a5b69a9e3892cfdf878ea6824d6c56b54
ghatikawaykole <ghatika99@gmail.com>
2021-09-22 13:00:09 +0530

    welcome stack compute

---
+++ src/com/example/DataStructureStack/DataStructureStatck.java
@@ -0,0 +1,7 @@
+package com.example.DataStructureStack;
+
+public class DataStructureStatck <E>{
+    public static void main(String args[]){
+        System.out.println("Welcome to stack implemetation use linkedlist");
+    }
+}
